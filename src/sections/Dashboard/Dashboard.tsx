f5ac5c695e7cef5488697a98476c137d5e698719
Whitev2 <WilliamHarris9584680@gmail.com>
2025-05-12 17:23:52 +0400

    u

--- src/sections/Dashboard/Dashboard.tsx
+++ src/sections/Dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { SlideSection } from '../../components/Slider'
 import { GAMES } from '../../games'
 import { GameCard } from './GameCard'
-import { WelcomeBanner } from './WelcomeBanner'
+// import { WelcomeBanner } from './WelcomeBanner'
 
 export function GameSlider() {
   return (
@@ -45,7 +45,7 @@ export function GameGrid() {
 export default function Dashboard() {
   return (
     <>
-      <WelcomeBanner />
+      {/*<WelcomeBanner />*/}
       <h2 style={{ textAlign: 'center' }}>Games</h2>
       <GameGrid />
     </>
